15b6bf31e10b9085194fca6f564a2579a4d7dee0
Alberto Islas <albertisfu@gmail.com>
2015-01-17 22:18:26 -0600

    comentarios en urls.py

--- system/urls.py
+++ system/urls.py
@@ -10,19 +10,19 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^$', 'home.views.home', name='home'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="my_login"),
-#URLS de customer para administrador
-    url(r'^administrator/customers/$', 'customers.views.customerAdmin', name='customerAdmin'),
-    url(r'^administrator/customers/(?P<username>\w+)/', 'customers.views.customerAdminDetail', name='customerAdminDetail'),
-    url(r'^administrator/payments/$', 'payments.views.paymentAdmin', name='paymentAdmin'),
-    url(r'^administrator/payments/verified/$', 'payments.views.paymentAdminDetail', name='paymentAdminDetail'),
+#URLS  para administrador
+    url(r'^administrator/customers/$', 'customers.views.customerAdmin', name='customerAdmin'),#Vista de clientes
+    url(r'^administrator/customers/(?P<username>\w+)/', 'customers.views.customerAdminDetail', name='customerAdminDetail'), #Detalle del cliente
+    url(r'^administrator/payments/$', 'payments.views.paymentAdmin', name='paymentAdmin'), #Vista de pagos
+    url(r'^administrator/payments/verified/$', 'payments.views.paymentAdminDetail', name='paymentAdminDetail'), #Verificar un pago
     #url(r'^administrator/customers/(?P<username>[\w\-]+)/', 'customers.views.customerAdminDetail', name='customerAdminDetail'),
 
 #URLS de customer para cliente
-    url(r'^customer/$', 'customers.views.customerCustomer', name='customerCustomer'),
-    url(r'^customer/proyect/(?P<proyect>\w+)/', 'customers.views.customerProyectDetail', name='customerProyectDetail'),
-    url(r'^customer/payments/$', 'payments.views.customerPayment', name='customerPayment'),
-    url(r'^customer/payment/(?P<payment>\w+)/', 'payments.views.customerPaymentDetail', name='customerPaymentDetail'),
-    url(r'^customer/payments/pay/$', 'payments.views.customerPaymentPay', name='customerPaymentPay'),
-    url(r'^customer/proyect-pay/(?P<proyect>\w+)/', 'payments.views.customerPaymentPayProyect', name='customerPaymentPayProyect'),
+    url(r'^customer/$', 'customers.views.customerCustomer', name='customerCustomer'), #Resumen de proyectos
+    url(r'^customer/proyect/(?P<proyect>\w+)/', 'customers.views.customerProyectDetail', name='customerProyectDetail'), #Detalle de proyecto
+    url(r'^customer/payments/$', 'payments.views.customerPayment', name='customerPayment'), #Resumen de pagos
+    url(r'^customer/payment/(?P<payment>\w+)/', 'payments.views.customerPaymentDetail', name='customerPaymentDetail'), #Detalle de pagos
+    url(r'^customer/payments/pay/$', 'payments.views.customerPaymentPay', name='customerPaymentPay'), #Realizar un pago general
+    url(r'^customer/proyect-pay/(?P<proyect>\w+)/', 'payments.views.customerPaymentPayProyect', name='customerPaymentPayProyect'), #Realizar un pago especifico
     
 )
