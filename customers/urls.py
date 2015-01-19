8d53218803bd752042c6f5c40964946691f992f5
Alberto Islas <albertisfu@gmail.com>
2015-01-19 01:11:34 -0600

    agregamos urls en /customers

---
+++ customers/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+	#URLS  para administrador
+	url(r'^administrator/customers/$', 'customers.views.customerAdmin', name='customerAdmin'),#Vista de clientes
+	url(r'^administrator/customers/(?P<username>\w+)/', 'customers.views.customerAdminDetail', name='customerAdminDetail'), #Detalle del cliente
+	#URLS de customer para cliente
+    url(r'^customer/$', 'customers.views.customerCustomer', name='customerCustomer'), #Resumen de proyectos
+    url(r'^customer/proyect/(?P<proyect>\w+)/', 'customers.views.customerProyectDetail', name='customerProyectDetail'), #Detalle de proyecto
+
+    )
--- system/urls.py
+++ system/urls.py
@@ -12,19 +12,12 @@ urlpatterns = patterns('',
     url(r'', include('customers.urls')),
     url(r'', include('payments.urls')),
 
-
     #url(r'^$', 'home.views.home', name='home'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="my_login"),
-#URLS  para administrador
-    url(r'^administrator/customers/$', 'customers.views.customerAdmin', name='customerAdmin'),#Vista de clientes
-    url(r'^administrator/customers/(?P<username>\w+)/', 'customers.views.customerAdminDetail', name='customerAdminDetail'), #Detalle del cliente
+
     url(r'^administrator/payments/$', 'payments.views.paymentAdmin', name='paymentAdmin'), #Vista de pagos
     url(r'^administrator/payments/verified/$', 'payments.views.paymentAdminDetail', name='paymentAdminDetail'), #Verificar un pago
     #url(r'^administrator/customers/(?P<username>[\w\-]+)/', 'customers.views.customerAdminDetail', name='customerAdminDetail'),
-
-#URLS de customer para cliente
-    url(r'^customer/$', 'customers.views.customerCustomer', name='customerCustomer'), #Resumen de proyectos
-    url(r'^customer/proyect/(?P<proyect>\w+)/', 'customers.views.customerProyectDetail', name='customerProyectDetail'), #Detalle de proyecto
     url(r'^customer/payments/$', 'payments.views.customerPayment', name='customerPayment'), #Resumen de pagos
     url(r'^customer/payment/(?P<payment>\w+)/', 'payments.views.customerPaymentDetail', name='customerPaymentDetail'), #Detalle de pagos
     url(r'^customer/payments/pay/$', 'payments.views.customerPaymentPay', name='customerPaymentPay'), #Realizar un pago general
