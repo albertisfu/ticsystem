dbddde5802fba065a4c60fd9e00d332fc0fd517f
Alberto Islas <albertisfu@gmail.com>
2015-03-15 16:49:41 -0600

    test

--- contents/forms.py
+++ contents/forms.py
@@ -15,12 +15,12 @@ class ContentForm(forms.ModelForm):
 		media = super(ContentForm, self).save(commit=False)
 		if commit:
 			media.save()
-		print self.instance.pk
+		#print self.instance.pk
 		inst = self.instance
 		file_ids = [x for x in self.cleaned_data.get('file_ids').split(',') if x]
-		print file_ids
+		#print file_ids
 		for file_id in file_ids:
-			print file_id  
+			#print file_id  
 			f = Picture.objects.get(id=file_id)
 			a = LogoUpload(content=inst,attachment=f)
 			a.save()
@@ -40,10 +40,10 @@ class SectionForm(forms.ModelForm):
 		media = super(SectionForm, self).save(commit=False)
 		if commit:
 			media.save()
-		print self.instance.pk
+		#print self.instance.pk
 		inst = self.instance
 		file_ids = [x for x in self.cleaned_data.get('file_ids').split(',') if x]
-		print file_ids
+		#print file_ids
 		for file_id in file_ids:
 			print file_id  
 			f = Picture.objects.get(id=file_id)
