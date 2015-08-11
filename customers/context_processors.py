face2e9de6f12f1231ce88651a5517a05f9c8bab
Alberto Islas <albertisfu@gmail.com>
2015-08-10 22:25:44 -0500

    context processors solve error

--- customers/context_processors.py
+++ customers/context_processors.py
@@ -1,11 +1,17 @@
 
 from django.contrib.auth.models import Permission, User
 from customers.models import Customer
+from django.contrib.auth.decorators import login_required
 
-
-def usuario(request):
-	current_user = request.user
-	customer= Customer.objects.get(user = current_user)
-        return {
+def usuario(request):  #procesador de contexto, manda una variable a todos los templates
+	if request.user.is_authenticated: #si la variable contiene datos de un usuario tenemos que verificar que este logueado para no causar errores en templates publicos
+		try:
+			current_user = request.user
+			customer= Customer.objects.get(user = current_user)
+			return {
             'usuario':customer.name,
+        }
+		except:
+			return {
+            'usuario':"", #forzozamente se tiene que retornar algo
         }
